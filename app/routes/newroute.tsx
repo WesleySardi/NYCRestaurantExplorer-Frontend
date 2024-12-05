6f9fc6a4ee6e6e22774e87a5da3b14bc3983bf29
Wesley Sardi <wesley.sardi@gmail.com>
2024-12-04 23:59:41 -0300

    Initial Structure

---
+++ app/routes/newroute.tsx
@@ -0,0 +1,8 @@
+export default function NewRoute() {
+  return (
+    <div>
+      <h1>NYC Restaurant Explorer</h1>
+      <p>Bem-vindo ao projeto de inspeção de restaurantes em Nova York!</p>
+    </div>
+  );
+}
